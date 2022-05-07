56a0a3aec909ef9095625d30c497e7335e000b9e
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-07 08:37:13 +0530

    Integrate event producer with controller.

--- src/main/java/com/ss/controller/LibraryEventsController.java
+++ src/main/java/com/ss/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.ss.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ss.domain.LibraryEvent;
+import com.ss.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         // invoke kafka producer
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
