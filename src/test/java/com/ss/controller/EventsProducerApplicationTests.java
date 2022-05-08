d2a5516c8841b81cec1f5f9a1d34aa3460bdb808
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-08 12:56:30 +0530

    Using Embedded Kafka for Junit tests

--- src/test/java/com/ss/controller/EventsProducerApplicationTests.java
+++ src/test/java/com/ss/controller/EventsProducerApplicationTests.java
@@ -7,29 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {
+        "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 class EventsProducerApplicationTests {
-	@Autowired
-	TestRestTemplate testRestTemplate;
+    @Autowired
+    TestRestTemplate testRestTemplate;
 
-	@Test
-	void postLibraryEvent() {
-		//given
-		Book book = Book.builder().bookId(123).bookName("test").bookAuthor("author").build();
-		LibraryEvent libraryEvent = LibraryEvent.builder().libraryEventId(null).book(book).build();
+    @Test
+    void postLibraryEvent() {
+        //given
+        Book book = Book.builder().bookId(123).bookName("test").bookAuthor("author").build();
+        LibraryEvent libraryEvent = LibraryEvent.builder().libraryEventId(null).book(book).build();
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("content-type", MediaType.APPLICATION_JSON.toString());
-		HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
 
-		//when
-		ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryevent-sync", HttpMethod.POST, request, LibraryEvent.class);
+        //when
+        ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryevent-sync", HttpMethod.POST, request, LibraryEvent.class);
 
-		//then
-		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-	}
+        //then
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
 
 }
