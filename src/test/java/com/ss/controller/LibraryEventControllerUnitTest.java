5b33da3f79b857a30f57492636d444ce9e46770e
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-08 16:35:16 +0530

    Adding custom error message using controllerAdvice

--- src/test/java/com/ss/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/com/ss/controller/LibraryEventControllerUnitTest.java
@@ -14,10 +14,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LibraryEventsController.class)
@@ -46,10 +48,36 @@ public class LibraryEventControllerUnitTest {
         String json = objectMapper.writeValueAsString(libraryEvent);
         doNothing().when(libraryEventProducer).sendLibraryEvent(isA(LibraryEvent.class));
 
-        //when
+        //expect
         mockMvc.perform(post("/v1/libraryevent")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+        //given
+        Book book = Book.builder()
+                .bookId(null)
+                .bookName(null)
+                .bookAuthor(null).build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book).build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent(isA(LibraryEvent.class));
+
+        //expect
+        String expectedErrorMessage = "book.bookAuthor - must not be blank, book.bookId - must not be null, book.bookName - must not be blank";
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
+
+    }
+
 }
