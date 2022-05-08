992c7704b4c2b6414a1af47308876ad275d13531
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-05-08 18:10:33 +0530

    Unit test case for eventProducer success case.

--- src/test/java/com/ss/producer/LibraryEventProducerUnitTest.java
+++ src/test/java/com/ss/producer/LibraryEventProducerUnitTest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ss.domain.Book;
 import com.ss.domain.LibraryEvent;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.SettableListenableFuture;
+import scala.Int;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -33,7 +38,7 @@ public class LibraryEventProducerUnitTest {
     LibraryEventProducer eventProducer;
 
     @Test
-    void sendLibraryEvent() throws ExecutionException, JsonProcessingException, InterruptedException, TimeoutException {
+    void sendLibraryEvent_failure() throws ExecutionException, JsonProcessingException, InterruptedException, TimeoutException {
         Book book = Book.builder()
                 .bookId(123)
                 .bookName("test")
@@ -48,5 +53,30 @@ public class LibraryEventProducerUnitTest {
         when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
 
         assertThrows(Exception.class, ()->eventProducer.sendLibraryEventSyncWithTopic(libraryEvent));
+    }
+
+    @Test
+    void sendLibraryEvent_success() throws ExecutionException, JsonProcessingException, InterruptedException, TimeoutException {
+        Book book = Book.builder()
+                .bookId(123)
+                .bookName("test")
+                .bookAuthor("author").build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book).build();
+
+        String record = objectMapper.writeValueAsString(libraryEvent);
+        SettableListenableFuture future = new SettableListenableFuture<>();
+
+        ProducerRecord<Integer,String> producerRecord = new ProducerRecord<>("library-events", libraryEvent.getLibraryEventId(), record);
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("library-events", 1), 1 ,1,324,1,1);
+        SendResult<Integer, String> sendResult = new SendResult<>(producerRecord, recordMetadata);
+
+        future.set(sendResult);
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
+
+        SendResult<Integer,String> result = eventProducer.sendLibraryEventSyncWithTopic(libraryEvent);
+        assert result.getRecordMetadata().partition()==1;
     }
 }
